73b8ffe473d93df7bd21d0e1cff24d2724d2203d
Helin Akdoğan <akdoganhelin@gmail.com>
2024-05-11 20:12:00 +0300

    encrypted chat fixed

--- TCPclient.py
+++ TCPclient.py
@@ -5,8 +5,6 @@ import os
 from cryptography.fernet import Fernet
 import base64
 
-
-
 with open('username.json', 'r') as file:
     user = json.load(file)
     username = user['username']
@@ -31,13 +29,12 @@ def display_available_users():
         available_users += f"{username} {status}\n"
     print(available_users)
 
-
 def save_chat_log(username, message, client):
     folder = 'logs'
     if not os.path.exists(folder):
         os.makedirs(folder)
     file_path = os.path.join(folder, f'{client}.txt')
-    with open(file_path,'a') as file:
+    with open(file_path, 'a') as file:
         file.write(f'{time.ctime()} | {username}: {message}\n')
 
 def history(client):
@@ -55,43 +52,35 @@ def history(client):
 def chat():
     chatInitiator = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_ID = input("who do you want to chat with? ")
-    addr = (data[client_ID][0],6001)
+    addr = (data[client_ID][0], 6001)
     chatInitiator.connect(addr)
     secure = input("1 for encrypted chat, 2 for unencrypted chat: ")
     if secure == '1':
         secretKey = int(input("choose your secret key: "))
-        key = ((5^secretKey)%23)
-        json_payload = {"user": username,
-                        "key": key,
-                        }
+        key = ((5^secretKey) % 23)
+        json_payload = {"user": username, "key": key}
         chatInitiator.send(json.dumps(json_payload).encode())      
-        tempkey = int(chatInitiator.recv(2048).decode())
-        finalkey = (tempkey^secretKey)%23
-        integer_bytes = int.to_bytes(finalkey, length=32, byteorder='big')
-        fernet_key = base64.urlsafe_b64encode(integer_bytes)
-        f = Fernet(fernet_key)
+        tempkey = chatInitiator.recv(2048).decode()
+        if tempkey:
+            finalkey = (int(tempkey) ^ secretKey) % 23
+            integer_bytes = int.to_bytes(finalkey, length=32, byteorder='big')
+            fernet_key = base64.urlsafe_b64encode(integer_bytes)
+            f = Fernet(fernet_key)
 
-        message = input('message: ')
-        encrypted_message = Fernet.encrypt(message)
-        json_payload = {"user": username,
-                        "encrypted message": encrypted_message,
-                        }
-        chatInitiator.send(json.dumps(json_payload).encode())
-        save_chat_log(username, message, client_ID)
+            message = input('message: ')
+            encrypted_message_bytes = f.encrypt(message.encode())
+            encrypted_message_base64 = base64.b64encode(encrypted_message_bytes).decode()
+            json_payload = {"user": username, "encrypted_message": encrypted_message_base64}
+            chatInitiator.send(json.dumps(json_payload).encode())
+            save_chat_log(username, message, client_ID)
     elif secure == '2':
         message = input("message: ")
         key = 0
         unencrypted_message = message
-        json_payload = {"user": username,
-                        "key": key,
-                        "unencrypted message": unencrypted_message,
-                        }
+        json_payload = {"user": username, "key": key, "unencrypted_message": unencrypted_message}
         chatInitiator.send(json.dumps(json_payload).encode())
         save_chat_log(username, message, client_ID)
     chatInitiator.close()
-
-    
-
 
 def mainMenu():
     print("1. Users")
--- TCPserver.py
+++ TCPserver.py
@@ -1,11 +1,11 @@
 import socket
-import time
 import json
+import time
 import os
 from cryptography.fernet import Fernet
 import base64
 
-chatResponder = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+chatResponder = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_address = ('', 6001)  # Use port 6001 for chat
 chatResponder.bind(server_address)
 chatResponder.listen(1)  # Listen for incoming connections (backlog of 1)
@@ -18,8 +18,9 @@ def save_chat_log(username, message):
     if not os.path.exists(folder):
         os.makedirs(folder)
     file_path = os.path.join(folder, f'{username}.txt')
-    with open(file_path,'a') as file:
+    with open(file_path, 'a') as file:
         file.write(f'{time.ctime()} | {username}: {message}\n')
+
 
 while True:
     # Accept incoming connection
@@ -27,24 +28,24 @@ while True:
     returnIP = client_address[0]
     json_payload = json.loads(client_socket.recv(1024).decode())
     username = json_payload['user']
-    if (json_payload['key']) == 0:
-        print(f"{username}: {json_payload['unencrypted message']}")
-        save_chat_log(username, json_payload['unencrypted message'])
+    if json_payload.get('key') == 0:
+        print(f"{username}: {json_payload.get('unencrypted_message')}")
+        save_chat_log(username, json_payload.get('unencrypted_message'))
         client_socket.close()
     else:
         secretkey = int(input("enter your key: "))
-        key = (5^secretkey)%23
+        key = (5 ^ secretkey) % 23
         client_socket.send(str(key).encode())
-        finalkey = (int(json_payload['key'])^secretkey)%23
+        finalkey = (int(json_payload['key']) ^ secretkey) % 23
         integer_bytes = int.to_bytes(finalkey, length=32, byteorder='big')
         fernet_key = base64.urlsafe_b64encode(integer_bytes)
         f = Fernet(fernet_key)
-        json_payload = json.loads(client_socket.recv(2048).decode())
-        message = json_payload["encrypted message"]
-        decrypted_message = f.decrypt(message)
-        print(f"{username}: {decrypted_message}")
-        save_chat_log(username, decrypted_message)
+        received_data = client_socket.recv(2048)
+        if received_data:
+            json_payload = json.loads(received_data.decode())
+            message = json_payload.get("encrypted_message")
+            if message:
+                decrypted_message = f.decrypt(base64.b64decode(message)).decode()
+                print(f"{username}: {decrypted_message}")
+                save_chat_log(username, decrypted_message)
         client_socket.close()
-
-
-
